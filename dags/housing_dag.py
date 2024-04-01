129b38abb8a14ba803cd3972b9c97d43988138aa
Giuseppe <giuseppe.fur96@gmail.com>
2024-04-01 20:31:18 +0200

    updates dags

--- dags/housing_dag.py
+++ dags/housing_dag.py
@@ -1,14 +1,17 @@
+import os
 import sys
 from datetime import datetime, timedelta
 from pathlib import Path
 
 from airflow import DAG
+from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
 
 sys.path.append(str(Path(__file__).parents[1]))
 
 from src.extract import extract_data  # noqa
 from src.load import load_data  # noqa
+from src.send_report import create_report  # noqa
 from src.transform import transform_data  # noqa
 
 default_args = {
@@ -50,4 +53,26 @@ load_task = PythonOperator(
     dag=dag,
 )
 
-extract_task >> transform_task >> load_task
+prepare_email_content = PythonOperator(
+    task_id="prepare_email_content",
+    python_callable=create_report,
+    dag=dag,
+    provide_context=True,  # Make sure to provide the context to use kwargs
+)
+
+send_email = EmailOperator(
+    task_id="send_report_email",
+    to=os.getenv("EMAIL"),
+    subject="Housing Report",
+    html_content="{{ ti.xcom_pull(task_ids='prepare_email_content') }}",
+    dag=dag,
+)
+
+
+(
+    extract_task
+    >> transform_task
+    >> load_task
+    >> prepare_email_content
+    >> send_email  # noqa
+)
